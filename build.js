302506431b1bb23d120b475f3c34c25fcceb805f
feederbox826 <feederbox826@users.noreply.github.com>
2024-07-18 13:51:51 -0400

    add cookie list

--- build.js
+++ build.js
@@ -1,8 +1,16 @@
 const Mustache = require("mustache");
 Mustache.escape = text => text // don't escape anything
 const fs = require("fs");
-const template = fs.readFileSync("filter.mustache", "utf8");
+
 const data = require("./filter.json")
 data.version = new Date().toISOString().split('T')[0]
-const result = Mustache.render(template, data)
-fs.writeFileSync("filter.txt", result, "utf8");
+
+// filter list
+const promoTemplate = fs.readFileSync("filter.mustache", "utf8");
+const promoResult = Mustache.render(promoTemplate, data)
+fs.writeFileSync("filter.txt", promoResult, "utf8");
+
+// cookie list
+const cookieTemplate = fs.readFileSync("cookie.mustache", "utf8");
+const cookieResult = Mustache.render(cookieTemplate, data)
+fs.writeFileSync("cookie.txt", cookieResult, "utf8");
